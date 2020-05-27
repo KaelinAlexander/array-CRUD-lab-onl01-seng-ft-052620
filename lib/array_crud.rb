04eea800eb1cee68bb286b23cf881f93453462b4
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-27 21:03:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  full
+  full_array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  full_array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
